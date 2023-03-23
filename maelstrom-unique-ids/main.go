06f5eb7f465223e87e4d17b1d6569becebbf1fac
Dharan Aditya <dharan.aditya@outlook.com>
2023-03-23 15:30:22 +0530

    Single Node implementation

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -3,17 +3,39 @@ package main
 import (
 	"encoding/json"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	"log"
+	"sync/atomic"
 )
 
 func main() {
 	n := maelstrom.NewNode()
-	n.Handle("generate", func(msg maelstrom.Message) error {
+	var idGenerator IdGenerator = &UniqueIdGeneratorService{}
+	n.Handle("generate", NewIdGeneratorHandler(idGenerator, n))
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewIdGeneratorHandler(idGenerator IdGenerator, n *maelstrom.Node) maelstrom.HandlerFunc {
+	return func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 		body["type"] = "generate_ok"
-		body["id"] = "FIXME"
+		body["id"] = idGenerator.Generate()
 		return n.Reply(msg, body)
-	})
+	}
+}
+
+type IdGenerator interface {
+	Generate() int64
+}
+
+type UniqueIdGeneratorService struct {
+	count int64
+}
+
+func (u *UniqueIdGeneratorService) Generate() int64 {
+	return atomic.AddInt64(&u.count, 1)
 }
